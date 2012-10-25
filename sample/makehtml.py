47ed193b8636a6bf47fa2755a68869bedb3056a3
Eli Finer <elig@il.ibm.com>
2012-10-25 20:33:03 +0200

    css based layout

--- sample/makehtml.py
+++ sample/makehtml.py
@@ -1,38 +1,38 @@
-print "<body dir='rtl'>"
-print """\
+with open("chat.html", "w") as f:
+    print >>f, """\
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <style>
-/*
-table, tr, td {
-    border-collapse: collapse;
-}
-*/
-td {
-    padding-top: 10px;
-    padding-bottom: 10px;
-    vertical-align: top;
-}
-td.message {
-    border-bottom: 1px solid #ccc;
-}
-td.name {
-    padding-left: 20px;
-    //color: #ccc;
+.name {
     font-weight: bold;
+    float: right;
+    clear: right;
+    width: 70px;
+}
+.text {
+    float: right;
+    margin-right: 10px;
+}
+.message {
+    padding: 5px 0 5px 0;
+    border-bottom: 1px solid #ccc;
+    float: right;
+    clear: right;
+    width: 600px;
 }
 </style>
-"""
-print "<table>"
-prevname = None
-message = []
-for name, text in eval(open("chat.data").read()):
-    name = name.decode("utf8")
-    text = text.decode("utf8")
-    message.append(text)
-    if not prevname:
-        prevname = name
-    if prevname and name != prevname:
-        print ("<tr><td class='name'>%s</td><td class='message'>%s</td></tr>" % (prevname, "<br>".join(message[:-1]))).encode("utf8")
-        message = message[-1:]
-        prevname = name
-print "</table>"
-print "</body>"
+    """
+    print >>f, "<body dir='rtl'>"
+    prevname = None
+    message = []
+    for name, text in eval(open("chat.data").read()):
+        name = name.decode("utf8")
+        text = text.decode("utf8")
+        message.append(text)
+        if not prevname:
+            prevname = name
+        if prevname and name != prevname:
+            print >>f, ("<div class='message'><div class='name'>%s</div><div class='text'>%s</div></div>" % 
+                (prevname, "<br>".join(message[:-1]))).encode("utf8")
+            message = message[-1:]
+            prevname = name
+    print >>f, "</body>"
